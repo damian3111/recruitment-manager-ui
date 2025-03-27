cdef6428ec11922f3b8309695cd6f067a1aab9f0
damian3111 <damiankwasny311@gmail.com>
2025-03-27 21:12:38 +0100

    add api axios

---
+++ src/utils/api.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+
+const api: AxiosInstance = axios.create({
+    baseURL: "http://localhost:8080/api",
+    withCredentials: true
+});
+
+// Attach token only when running on the client side
+// api.interceptors.request.use(
+//     (config) => {
+//         if (typeof window !== "undefined") {
+//             const token = localStorage.getItem("authToken");
+//             if (token && config.headers) {
+//                 config.headers["Authorization"] = `Bearer ${token}`;
+//             }
+//         }
+//         return config;
+//     },
+//     (error) => Promise.reject(error)
+// );
+
+export default api;
