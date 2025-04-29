fe0cd80cb3108eca7f3c98720e633dcab1087ef2
damian3111 <damiankwasny311@gmail.com>
2025-04-29 18:19:31 +0200

    create userService

---
+++ src/lib/userService.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from '../utils/api';
+
+export const useCurrentUser = () => {
+  return useQuery({
+    queryKey: ['currentUser'],
+    queryFn: async () => {
+      const res = await axios.get('/users/me');
+      return res.data;
+    },
+    staleTime: 1000 * 60 * 5,
+  });
+};
