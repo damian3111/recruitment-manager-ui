50966644b14d6d5b73adb3b5577cc83e250331c9
damian3111 <damiankwasny311@gmail.com>
2025-05-22 16:54:50 +0200

    all changes jobs/page.tsx

--- src/app/(dashboard)/invitations/page.tsx
+++ src/app/(dashboard)/invitations/page.tsx
@@ -7,6 +7,7 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card'
+import { motion } from 'framer-motion';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Header } from '@/components/header'
 import { Main } from '@/components/main'
@@ -30,6 +31,8 @@ import {
 } from "@/lib/invitationService";
 import {Badge} from "@/components/ui/badge";
 import toast from "react-hot-toast";
+import UnderConstructionPage from "@/components/UnderConstructionPage";
+import {Wrench} from "lucide-react";
 
 export default function Dashboard() {
     const { data: user } = useCurrentUser()
@@ -119,6 +122,15 @@ export default function Dashboard() {
         });
     };
 
+    const handleRemoveRelation = (inviteId: number) => {
+        deleteInvite.mutate(inviteId, {
+            onSuccess: () => {
+                toast.success('✅ Invitation deleted!');
+            },
+            onError: () => toast.error('❌ Failed to delete invitation.'),
+        });
+    };
+
     return (
         <>
             {/* ===== Top Heading ===== */}
@@ -146,7 +158,6 @@ export default function Dashboard() {
                 >
                     <div className='w-full overflow-x-auto pb-2'>
                         <TabsList>
-                            <TabsTrigger value='overview'>Overview</TabsTrigger>
                             <TabsTrigger value='analytics'>
                                 Analytics
                             </TabsTrigger>
@@ -156,134 +167,11 @@ export default function Dashboard() {
                             <TabsTrigger value='accepted'>
                                 Accepted
                             </TabsTrigger>
+                            <TabsTrigger value='favourite'>
+                                Favourite
+                            </TabsTrigger>
                         </TabsList>
                     </div>
-                    <TabsContent value='overview' className='space-y-4'>
-                        <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-4'>
-                            <Card>
-                                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-                                    <CardTitle className='text-sm font-medium'>
-                                        Total Revenue
-                                    </CardTitle>
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        viewBox='0 0 24 24'
-                                        fill='none'
-                                        stroke='currentColor'
-                                        strokeLinecap='round'
-                                        strokeLinejoin='round'
-                                        strokeWidth='2'
-                                        className='text-muted-foreground h-4 w-4'
-                                    >
-                                        <path d='M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6' />
-                                    </svg>
-                                </CardHeader>
-                                <CardContent>
-                                    <div className='text-2xl font-bold'>$45,231.89</div>
-                                    <p className='text-muted-foreground text-xs'>
-                                        +20.1% from last month
-                                    </p>
-                                </CardContent>
-                            </Card>
-                            <Card>
-                                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-                                    <CardTitle className='text-sm font-medium'>
-                                        Subscriptions
-                                    </CardTitle>
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        viewBox='0 0 24 24'
-                                        fill='none'
-                                        stroke='currentColor'
-                                        strokeLinecap='round'
-                                        strokeLinejoin='round'
-                                        strokeWidth='2'
-                                        className='text-muted-foreground h-4 w-4'
-                                    >
-                                        <path d='M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2' />
-                                        <circle cx='9' cy='7' r='4' />
-                                        <path d='M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75' />
-                                    </svg>
-                                </CardHeader>
-                                <CardContent>
-                                    <div className='text-2xl font-bold'>+2350</div>
-                                    <p className='text-muted-foreground text-xs'>
-                                        +180.1% from last month
-                                    </p>
-                                </CardContent>
-                            </Card>
-                            <Card>
-                                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-                                    <CardTitle className='text-sm font-medium'>Sales</CardTitle>
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        viewBox='0 0 24 24'
-                                        fill='none'
-                                        stroke='currentColor'
-                                        strokeLinecap='round'
-                                        strokeLinejoin='round'
-                                        strokeWidth='2'
-                                        className='text-muted-foreground h-4 w-4'
-                                    >
-                                        <rect width='20' height='14' x='2' y='5' rx='2' />
-                                        <path d='M2 10h20' />
-                                    </svg>
-                                </CardHeader>
-                                <CardContent>
-                                    <div className='text-2xl font-bold'>+12,234</div>
-                                    <p className='text-muted-foreground text-xs'>
-                                        +19% from last month
-                                    </p>
-                                </CardContent>
-                            </Card>
-                            <Card>
-                                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-                                    <CardTitle className='text-sm font-medium'>
-                                        Active Now
-                                    </CardTitle>
-                                    <svg
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        viewBox='0 0 24 24'
-                                        fill='none'
-                                        stroke='currentColor'
-                                        strokeLinecap='round'
-                                        strokeLinejoin='round'
-                                        strokeWidth='2'
-                                        className='text-muted-foreground h-4 w-4'
-                                    >
-                                        <path d='M22 12h-4l-3 9L9 3l-3 9H2' />
-                                    </svg>
-                                </CardHeader>
-                                <CardContent>
-                                    <div className='text-2xl font-bold'>+573</div>
-                                    <p className='text-muted-foreground text-xs'>
-                                        +201 since last hour
-                                    </p>
-                                </CardContent>
-                            </Card>
-                        </div>
-                        <div className='grid grid-cols-1 gap-4 lg:grid-cols-7'>
-                            <Card className='col-span-1 lg:col-span-4'>
-                                <CardHeader>
-                                    <CardTitle>Overview</CardTitle>
-                                </CardHeader>
-                                <CardContent className='pl-2'>
-                                    <Overview />
-                                </CardContent>
-                            </Card>
-                            <Card className='col-span-1 lg:col-span-3'>
-                                <CardHeader>
-                                    <CardTitle>Recent Sales</CardTitle>
-                                    <CardDescription>
-                                        You made 265 sales this month.
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    <RecentSales />
-                                </CardContent>
-                            </Card>
-                        </div>
-                    </TabsContent>
                     <TabsContent value='analytics' className='space-y-4'>
                         <div className="space-y-4">
                             <Card className="p-6 rounded-2xl shadow-md border border-gray-200 bg-white">
@@ -538,22 +426,16 @@ export default function Dashboard() {
                                                                     View Candidate
                                                                 </Link>
                                                             )}
-                                                            {/*{candidate && job && (*/}
-                                                            {/*    <div>*/}
-                                                            {/*        <button*/}
-                                                            {/*            onClick={() => handleAcceptInvitation(job.id, candidate.id)}*/}
-                                                            {/*            className={`px-3 py-1 rounded text-sm transition bg-red-500 text-white hover:bg-red-600`}*/}
-                                                            {/*        >*/}
-                                                            {/*            Accept Invitation*/}
-                                                            {/*        </button>*/}
-                                                            {/*        <button*/}
-                                                            {/*            onClick={() => handleCancelAnalytics(job.id, candidate.id)}*/}
-                                                            {/*            className={`px-3 py-1 rounded text-sm transition bg-red-500 text-white hover:bg-red-600`}*/}
-                                                            {/*        >*/}
-                                                            {/*            Cancel Invitation*/}
-                                                            {/*        </button>*/}
-                                                            {/*    </div>*/}
-                                                            {/*)}*/}
+                                                            {candidate && job && (
+                                                                <div>
+                                                                    <button
+                                                                        onClick={() => handleRemoveRelation(invite.id)}
+                                                                        className={`px-3 py-1 rounded text-sm transition bg-red-500 text-white hover:bg-red-600`}
+                                                                    >
+                                                                        Remove Relation
+                                                                    </button>
+                                                                </div>
+                                                            )}
                                                         </td>
                                                     </tr>
                                                 )
@@ -562,6 +444,11 @@ export default function Dashboard() {
                                     </table>
                                 </div>
                             </Card>
+                        </div>
+                    </TabsContent>
+                    <TabsContent value='favourite' className='space-y-4'>
+                        <div className="space-y-4">
+                            <UnderConstructionPage/>
                         </div>
                     </TabsContent>
                 </Tabs>
--- src/app/(dashboard)/jobs/page.tsx
+++ src/app/(dashboard)/jobs/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Spinner } from '@/components/icons';
 import { Button } from '@/components/ui/button';
@@ -20,7 +20,13 @@ import {CandidateType, useCandidateByEmail} from "@/lib/candidatesService";
 import JobSelectModal from "@/components/JobSelectModal";
 import ConfirmModal from "@/components/confirmationModal";
 import toast from "react-hot-toast";
-import {useDeleteInvite, useInvitesByRecruiter, useSendInvite} from "@/lib/invitationService";
+import {
+    useDeleteInvite,
+    useInvites,
+    useInvitesByRecruiter,
+    useSendInvite,
+    useUpdateInviteStatus, useUserRelatedInvitations
+} from "@/lib/invitationService";
 import {useCurrentUser} from "@/lib/userService";
 
 export default function JobsPage() {
@@ -32,20 +38,41 @@ export default function JobsPage() {
     const sendInvite = useSendInvite();
     const { data: user } = useCurrentUser();
     const {data: candidate, refetch} = useCandidateByEmail(user?.email);
-    const { data: invites } = useInvitesByRecruiter(user?.id)
+    // const { data: invites } = useInvitesByRecruiter(user?.id);
+    const {data: invites} = useUserRelatedInvitations(user?.id, user?.email);
+
+    // const { data: invites } = useInvitesByRecruiter(user?.id)
     const deleteInvite = useDeleteInvite();
 
-    const { data: jobs, isLoading, isError, error } = useFilteredJobs(debouncedFilters);
+    // const { data: jobs, isLoading, isError, error } = useFilteredJobs(debouncedFilters);
     const [sentJobIds, setSentJobIds] = useState<number[]>([]);
+    const [acceptedJobIds, setAcceptedJobIds] = useState<number[]>([]);
+    const [receivedJobIds, setReceivedJobIds] = useState<number[]>([]);
+    const { mutate } = useUpdateInviteStatus();
+    const [bookmarkedJobIds, setBookmarkedJobIds] = useState<number[]>([]);
+    const [page, setPage] = useState(0);
+    const pageSize = 10;
+    const { data: jobs, isLoading, isError, error } = useFilteredJobs(debouncedFilters, page, pageSize);
 
     useEffect(() => {
         if (invites) {
             const sentIds = invites
-                .filter(invite => invite.status === 'sent')
+                .filter(invite => invite.status === 'sent' && invite.candidate_id == candidate?.id && invite.recruiter_id == user?.id)
                 .flatMap(invite => invite.job_id);
             setSentJobIds(sentIds);
+
+            const acceptedIds = invites
+                .filter(invite => invite.status === 'accepted' && invite.candidate_id == candidate?.id)
+                .flatMap(invite => invite.job_id);
+            setAcceptedJobIds(acceptedIds);
+
+            const receivedIds = invites
+                .filter(invite => invite.status === 'sent' && invite.candidate_id == candidate?.id && invite.recruiter_id != user?.id)
+                .flatMap(invite => invite.job_id);
+            setReceivedJobIds(receivedIds);
         }
-    }, [invites]);
+
+    }, [invites, candidate]);
 
     if (isLoading) {
         return (
@@ -64,7 +91,13 @@ export default function JobsPage() {
             </p>
         );
     }
-
+    const toggleBookmark = (jobId: number) => {
+        setBookmarkedJobIds(prev =>
+            prev.includes(jobId)
+                ? prev.filter(id => id !== jobId)
+                : [...prev, jobId]
+        );
+    };
     const handleSendInvitationClick = (job: JobType) => {
         setSelectedJobId(job.id);
         setShowConfirm(true);
@@ -89,6 +122,46 @@ export default function JobsPage() {
         });
     };
 
+    const handleAcceptInvitation = (jobId: number) => {
+        console.log("(((((");
+
+        const invite = invites?.find(inv =>
+            inv.status === 'sent' &&
+            inv.job_id === jobId &&
+            inv.candidate_id === candidate?.id
+        );
+        if (!invite) return;
+
+        mutate({
+            id: invite.id,
+            status: 'accepted',
+        }, {
+            onSuccess: () => {
+                toast.success('✅ Invitation accepted!');
+            },
+            onError: () => {
+                toast.error('❌ Failed to accept the invitation.');
+            },
+        });
+    };
+    const handleRemoveRelation = (jobId: number) => {
+        const invite = invites?.find(inv =>
+            inv.status === 'accepted' &&
+            inv.job_id === jobId &&
+            inv.candidate_id === candidate?.id
+        );
+
+        if (!invite) return;
+
+        deleteInvite.mutate(invite.id, {
+            onSuccess: () => {
+                setSentJobIds(prev => prev.filter(id => id !== jobId));
+                toast.success('✅ Invitation deleted!');
+                refetch(); // jeśli potrzebne
+            },
+            onError: () => toast.error('❌ Failed to delete invitation.'),
+        });
+    };
     const confirmAction = () => {
         sendInvite.mutate(
             {
@@ -123,7 +196,7 @@ export default function JobsPage() {
 
             {/* Job Cards */}
             <div className="w-3/4 space-y-4">
-                {jobs.content?.map((job: JobType) => (
+                {jobs?.content?.map((job: JobType) => (
                     <Card
                         key={job.id}
                         className="w-full p-4 rounded-2xl shadow-md border border-gray-200 bg-white hover:shadow-lg transition-shadow duration-300"
@@ -145,12 +218,34 @@ export default function JobsPage() {
                                         <Button size="sm" onClick={() => handleCancel(job)}>
                                             Sent
                                         </Button>
-                                    ) : (
+                                    ) : acceptedJobIds?.includes(job.id) ? (
+                                        <div>
+                                            <Button size="sm" onClick={() => handleCancel(job)}>
+                                                Invitation Accepted
+                                            </Button>
+                                            <Button
+                                                onClick={() => handleRemoveRelation(job.id)}
+                                                className={`px-3 py-1 rounded text-sm transition bg-red-500 text-white hover:bg-red-600`}
+                                            >
+                                                Remove Relation
+                                            </Button>
+                                        </div>
+                                        ) : receivedJobIds?.includes(job.id) ? (
+                                            <Button size="sm" onClick={() => handleAcceptInvitation(job.id)}>
+                                                Accept Invitation
+                                            </Button>
+                                        ) : (
                                         <Button size="sm" onClick={() => handleSendInvitationClick(job)}>
                                             I'm Interested
                                         </Button>
                                     )}
-                                    <Bookmark className="h-6 w-6" />
+                                    <button onClick={() => toggleBookmark(job.id)}>
+                                        <Bookmark
+                                            className="h-6 w-6 transition-colors duration-300"
+                                            stroke={bookmarkedJobIds.includes(job.id) ? "gold" : "black"}
+                                            fill={bookmarkedJobIds.includes(job.id) ? "gold" : "none"}
+                                        />
+                                    </button>
                                 </div>
                             )}
                             <DropdownMenu>
@@ -167,8 +262,28 @@ export default function JobsPage() {
                                 </DropdownMenuContent>
                             </DropdownMenu>
                         </div>
+
                     </Card>
                 ))}
+                {jobs && (
+                    <div className="flex justify-center gap-4 mt-6">
+                        <Button
+                            onClick={() => setPage(prev => Math.max(prev - 1, 0))}
+                            disabled={page === 0}
+                        >
+                            Previous
+                        </Button>
+                        <span className="flex items-center px-2">
+      Page {page + 1} of {jobs.totalPages}
+    </span>
+                        <Button
+                            onClick={() => setPage(prev => Math.min(prev + 1, jobs.totalPages - 1))}
+                            disabled={page >= jobs.totalPages - 1}
+                        >
+                            Next
+                        </Button>
+                    </div>
+                )}
                 <ConfirmModal
                     open={showConfirm}
                     onCancel={() => setShowConfirm(false)}
