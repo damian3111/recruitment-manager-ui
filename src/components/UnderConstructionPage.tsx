c6f405b7c25cb8e6bda471973d1d9b1ec35395a3
damian3111 <damiankwasny311@gmail.com>
2025-05-30 14:03:55 +0200

    add UnderConstrucionPage

---
+++ src/components/UnderConstructionPage.tsx
@@ -0,0 +1,28 @@
+// app/under-construction/page.tsx or any route you want
+
+'use client';
+
+import { motion } from 'framer-motion';
+import {Construction, Wrench} from 'lucide-react';
+import React from "react";
+
+export default function UnderConstructionPage() {
+    return (
+        <div className=" p-52 px-52 flex flex-col items-center justify-center bg-gradient-to-br from-gray-50 to-gray-200 px-4">
+            <motion.div
+                initial={{ y: 0 }}
+                animate={{ y: [0, -10, 0] }}
+                transition={{ repeat: Infinity, duration: 1.5 }}
+                className="mb-6"
+            >
+                <Wrench className="w-16 h-16 text-gray-700" />
+            </motion.div>
+            <h1 className="text-4xl font-bold text-gray-800 mb-2 text-center">
+                Page Under Construction
+            </h1>
+            <p className="text-gray-600 text-lg text-center max-w-md">
+                We're currently working on this page to make it perfect for you. Please check back soon!
+            </p>
+        </div>
+    );
+}
