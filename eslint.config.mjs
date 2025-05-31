9e00f4de1549b6127c14a598f6916c34c4cea04d
damian3111 <damiankwasny311@gmail.com>
2025-05-31 14:06:44 +0200

    config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    files: ["**/*.{js,cjs,mjs,ts,tsx}"], // <--- rozszerzenia plików
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+  },
+  ...compat.extends("next/core-web-vitals"),
+  ...compat.extends("next/typescript"),
 ];
-
-export default eslintConfig;
